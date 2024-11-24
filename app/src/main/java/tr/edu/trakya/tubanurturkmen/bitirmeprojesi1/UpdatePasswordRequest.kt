1a6fcb919406d039fa4f1741e2188583424c39cf
zehra gizem duman <zgduman@gmail.com>
2024-11-25 00:09:12 +0300

    fix

--- app/src/main/java/tr/edu/trakya/tubanurturkmen/bitirmeprojesi1/UpdatePasswordRequest.kt
+++
@@ -1,5 +0,0 @@
-package tr.edu.trakya.tubanurturkmen.bitirmeprojesi1
-
-data class UpdatePasswordRequest(
-    val password: String // The new password (TC Kimlik Numarası in this case)
-)
--- app/src/main/java/tr/edu/trakya/tubanurturkmen/bitirmeprojesi1/UpdatePasswordResponse.kt
+++
@@ -1,6 +0,0 @@
-package tr.edu.trakya.tubanurturkmen.bitirmeprojesi1
-
-data class UpdatePasswordResponse(
-    val success: Boolean, // This can be a simple boolean to indicate if the update was successful
-    val message: String // You can add a message if needed
-)
