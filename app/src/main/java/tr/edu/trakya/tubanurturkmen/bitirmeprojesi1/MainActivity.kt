7073ce5ebc3851cf765a3f22a32527ce0d85730f
zehra gizem duman <zgduman@gmail.com>
2024-12-15 17:05:11 +0300

    harita eklendi.

--- app/src/main/java/tr/edu/trakya/tubanurturkmen/bitirmeprojesi1/MainActivity.kt
+++ app/src/main/java/tr/edu/trakya/tubanurturkmen/bitirmeprojesi1/MainActivity.kt
@@ -1,9 +1,11 @@
 package tr.edu.trakya.tubanurturkmen.bitirmeprojesi1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import org.osmdroid.config.Configuration
 import tr.edu.trakya.tubanurturkmen.bitirmeprojesi1.ui.navigation.AppNavigation
 import tr.edu.trakya.tubanurturkmen.bitirmeprojesi1.ui.theme.BitirmeProjesi1Theme
 
@@ -11,6 +13,10 @@ import tr.edu.trakya.tubanurturkmen.bitirmeprojesi1.ui.theme.BitirmeProjesi1Them
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Configuration.getInstance().userAgentValue = "tr.edu.trakya.tubanurturkmen.bitirmeprojesi1"
+        // Log the current user agent value to the Logcat
+        Log.d("UserAgent", "User Agent: ${Configuration.getInstance().userAgentValue}")
+        // Harita bileşenini başlatıyoruz
         enableEdgeToEdge()
         setContent {
             BitirmeProjesi1Theme {
